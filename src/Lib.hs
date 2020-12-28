5ee8252d09da569a58495177c54c93f5dc12704a
Luke Weber <lukedottec@gmail.com>
2020-12-27 16:31:19 -0800

    Nit.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE ViewPatterns #-}
+
 module Lib
     ( someFunc
     ) where
 
+-- Fibonacci
 fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
+-- ...
 someFunc :: IO ()
-x = 20
+x = 22
 someFunc = putStrLn (show (fib x))
+
+-- ...
+
